2aa6773d11e6da3dc9d97a5274baf2f306fdd16a
S4N1DHY4 <98018508+S4N1DHY4@users.noreply.github.com>
2022-05-08 13:28:32 +0530

    MySQL-C++ Connection

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <windows.h>
-#include <mysql.h>
+#include <C:\Users\krish\Downloads\mysqlheaders\mysql.h>
 #include <sstream>
-#include<iomanip>
+#include <iomanip>
 using namespace std;
 
 const char* hostname="localhost";
@@ -19,88 +19,39 @@ MYSQL* connectdatabase()
     conn=mysql_init(0);
     conn=mysql_real_connect(conn,hostname,username,password,database,port,unixsocket,clientflag);
     if(conn)
-        {
-            cout<<"connected"<<endl;
-            return conn;
-        }
+    {
+        cout<<"connected"<<endl;
+        return conn;
+    }
     else
-        {
-            cout<<"not connected";
-            return conn;
-        }
+    {
+        cout<<"not connected"<<endl;
+        return conn;
+    }
+
+
 }
-void insertion (MYSQL* conn)
+void insertion(MYSQL* conn)
 {
-    int qstate = 0;
+    int qstate=0;
+    stringstream ss;
     string usernamedb,emaildb,passworddb;
-    cout<<"Enter username: ";
+    cout<<"Enter username:";
     cin>>usernamedb;
-    cout<<"Enter email id: ";
+    cout<<"Enter email id:";
     cin>>emaildb;
-    cout<<"Enter password: ";
+    cout<<"Enter password:";
     cin>>passworddb;
-    stringstream ss;
-    ss<<"INSERT INTO users (username, email, password) VALUES ('"+usernamedb+"','"+emaildb+"','"+passworddb+"')";
-    string query = ss.str();
-    const char* q = query.c_str();
-    qstate = mysql_query(conn,q);
-    if(qstate == 0)
-    {
-        cout<<"Record inserted...~~~ \n";
-    }
+    ss<<"INSERT INTO users (username,email,password) VALUES ('"+usernamedb+"','"+emaildb+"','"+passworddb+"')";
+    string query=ss.str();
+    const char* q=query.c_str();
+    qstate=mysql_query(conn,q);
+    if(qstate==0)
+        cout<<"Record inserted"<<endl;
     else
-    {
-        cout<<"Failed to insert...~~~ \n";
-    }
+        cout<<"Record insertion failed"<<endl;
 }
-void update (MYSQL* conn)
-{
-    int qstate;
-    string usernamedb,emaildb,passworddb;
-    int ID;
-    cout<<"Enter id to be updated:";
-    cin>>ID;
-    cout<<"Enter new username: ";
-    cin>>usernamedb;
-    cout<<"Enter new email id: ";
-    cin>>emaildb;
-    cout<<"Enter new password: ";
-    cin>>passworddb;
-    stringstream ss;
-    ss<<"UPDATE users SET username='"+usernamedb+"',email='"+emaildb+"',password='"+passworddb+"' WHERE id="<<ID;
-    string query = ss.str();
-    const char* q = query.c_str();
-    qstate = mysql_query(conn,q);
-    if(qstate == 0)
-    {
-        cout<<"Record updated...~~~ \n";
-    }
-    else
-    {
-        cout<<"Failed to update...~~~ \n";
-    }
-}
-void del(MYSQL* conn)
-{
-    int qstate;
-    string usernamedb,emaildb,passworddb;
-    int ID;
-    cout<<"Enter id to be deleted:";
-    cin>>ID;
-    stringstream ss;
-    ss<<"DELETE FROM users WHERE id="<<ID;
-    string query = ss.str();
-    const char* q = query.c_str();
-    qstate = mysql_query(conn,q);
-    if(qstate == 0)
-    {
-        cout<<"Record deleted...~~~ \n";
-    }
-    else
-    {
-        cout<<"Failed to delete...~~~ \n";
-    }
-}
+
 void display(MYSQL* conn)
 {
     MYSQL_ROW row;
@@ -108,14 +59,16 @@ void display(MYSQL* conn)
     MYSQL_FIELD* column;
     if(conn)
     {
-        int qstate = mysql_query(conn,"SELECT * FROM users");
+        int qstate=mysql_query(conn,"SELECT * FROM users");
         if(!qstate)
         {
             res=mysql_store_result(conn);
-            int count = mysql_num_fields(res);
-            column = mysql_fetch_field(res);
-            for(int i=1;i<=52;i++)
+            int count=mysql_num_fields(res);
+            column=mysql_fetch_field(res);
+            for(int i=0;i<52;i++)
+            {
                 cout<<"=";
+            }
             cout<<endl;
             for(int i=0; i<count; i++)
             {
@@ -123,27 +76,80 @@ void display(MYSQL* conn)
                 column = mysql_fetch_field(res);
             }
             cout<<endl;
-            column = mysql_fetch_field(res);
-            for(int i=1; i<=52; i++)
-                cout<<"=";
-            cout<<endl;
-            while(row=mysql_fetch_row(res))
+            column=mysql_fetch_field(res);
+            for(int i=0;i<52;i++)
             {
-                for(int i=0; i<count; i++)
+                cout<<"=";
+            }
+            cout<<endl;
+            while((row=mysql_fetch_row(res)))
+            {
+                for(int i=0;i<count;i++)
+                {
                     cout<<left<<setw(12)<<setfill(' ')<<row[i]<<"|";
+                }
                 cout<<endl;
             }
-            for(int i=1; i<=52; i++)
+            for(int i=0;i<52;i++)
+            {
                 cout<<"=";
+            }
             cout<<endl;
         }
     }
+    else
+    {
+        cout<<"Fetching failed"<<endl;
+    }
 }
+
+void update(MYSQL* conn)
+{
+    int qstate=0;
+    int ID;
+    cout<<"Enter id to be updated:";
+    cin>>ID;
+    string n_username,n_email;
+    cout<<"Enter new username:";
+    cin>>n_username;
+    cout<<"Enter new email id:";
+    cin>>n_email;
+    stringstream ss;
+    ss<<"UPDATE users SET username='"+n_username+"',email='"+n_email+"' WHERE id="<<ID;
+    string query=ss.str();
+    const char* q=query.c_str();
+    qstate=mysql_query(conn,q);
+    if(qstate==0)
+        cout<<"Record updated"<<endl;
+    else
+        cout<<"Record updation failed"<<endl;
+}
+
+void del(MYSQL* conn)
+{
+    int ID,qstate=0;
+    cout<<"Enter id to be deleted:";
+    cin>>ID;
+    stringstream ss;
+    ss<<"DELETE FROM users WHERE id="<<ID;
+    string query=ss.str();
+    const char* q=query.c_str();
+    qstate=mysql_query(conn,q);
+    if(qstate==0)
+        cout<<"Record deleted"<<endl;
+    else
+        cout<<"Record deletion failed"<<endl;
+}
+
 int main()
 {
-    MYSQL* conn = connectdatabase();
+    MYSQL* conn=connectdatabase();
     //insertion(conn);
-    //update(conn);
     display(conn);
+    update(conn);
+    display(conn);
+    //insertion(conn);
+    //del(conn);
+    //display(conn);
     return 0;
 }
